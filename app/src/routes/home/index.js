f32e7338de49b5ac97e035956d6bec1827ab7149
Chahyun <ckgus265@gmail.com>
2022-07-03 04:05:23 +0900

    HTML에서 값을 받는 API 활성화

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,11 +1,13 @@
 "use strict"
 const express = require("express");
 const router = express.Router();
+
 const ctrl = require("./home.ctrl")
 
-router.get("/", ctrl.hello);
+router.get("/", ctrl.output.home);
 
-router.get("/login", ctrl.login);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
 //외부로 내보내기 명령
 module.exports = router;
