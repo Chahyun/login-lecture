3966d416df1de36db42a55924999b666ace50565
Chahyun <ckgus265@gmail.com>
2022-07-03 02:18:18 +0900

    깃 저장소 초기화

---
+++ routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict"
+const express = require("express");
+const router = express.Router();
+const ctrl = require("./home.ctrl")
+
+router.get("/", ctrl.hello);
+
+router.get("/login", ctrl.login);
+
+//외부로 내보내기 명령
+module.exports = router;
