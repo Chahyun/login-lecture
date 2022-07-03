e31ec11ef13fe8ffb8dfb0cf9c4d4ca2dc0c1593
Chahyun <ckgus265@gmail.com>
2022-07-03 21:00:31 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,40 +1,45 @@
 "use strict"
 
-class UserStorage{
-   static #users = {
-        id : ["test","test1","test2"],
-        userName: ["testn","testn1","testn2"],
-        password : ["pass","pass1","pass2"],
-    };
+const fs = require("fs").promises;
 
-    static getUsers(...fields){
-        const users = this.#users;
-        const newUsesrs = fields.reduce((newUsesrs, field)=>{
-            if(users.hasOwnProperty(field)){
-                newUsesrs[field] = users[field];
-            }
-            return newUsesrs;
-        },{});
-        return newUsesrs;
-    }
-
-    static getUserInfo(id) {
-        const users = this.#users;
+class UserStorage {
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const userKeys = Object.keys(users)
-        const userInfo = userKeys.reduce((newUser,info) =>{
+        const userInfo = userKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
         return userInfo;
     }
 
+    static getUsers(...fields) {
+        const newUsesrs = fields.reduce((newUsesrs, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsesrs[field] = users[field];
+            }
+            return newUsesrs;
+        }, {});
+        return newUsesrs;
+    }
+
+    static getUserInfo(id) {
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);   //promise 반환
+
+    }
+
+
     static save(userInfo) {
-        const users = this.#users;
         users.id.push(userInfo.id);
         users.userName.push(userInfo.userName);
         users.password.push(userInfo.password);
-        return {success:true};
+        return { success: true };
     }
 }
 
