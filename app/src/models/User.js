da242e42b3c9f6ef51afd885395b0ffff2c6b08c
Chahyun <ckgus265@gmail.com>
2022-07-06 03:14:55 +0900

    백엔드 맛보기 최종

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,9 +9,9 @@ class User {
     async login() {
         const client = this.body
         try {
-            const { id, password } = await UserStorage.getUserInfo(client.id);
-            if (id) {
-                if (id === client.id && password === client.password) {
+            const user = await UserStorage.getUserInfo(client.id);
+            if (user) {
+                if (user.id === client.id && user.password === client.password) {
                     return { success: true };
                 } return { success: false, msg: "비밀번호가 일치하지 않습니다." };
             } return { success: false, msg: "존재하는 아이디가 없습니다." };
